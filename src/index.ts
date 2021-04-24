3343419abc8b702ac30528996e2f8c2dd8c33a69
qing1hao <124031296+qing1hao@users.noreply.github.com>
2021-04-24 08:14:38 +0000

    Bump version to 0.10

--- src/index.ts
+++ src/index.ts
@@ -321,4 +321,4 @@ function css(duration: number) {
     transform: translate(-50%, -50%);
     padding: 0 2vw;
     aspect-ratio: 9 / 16;
-    height
+    height: min(var(--magic-h), var(--magic-w) * 
