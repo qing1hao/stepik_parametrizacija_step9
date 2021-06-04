42116730fffb932eb3e4a43dbb1b70de612d3f15
qing1hao <124031296+qing1hao@users.noreply.github.com>
2021-06-04 21:23:16 +0000

    documentation and cleanup

--- src/index.ts
+++ src/index.ts
@@ -405,4 +405,6 @@ class OpenStoriesElement extends HTMLElement {
     this.root.innerHTML = `
       <button type="dialog" id="trigger" part="button"><slot>View stories</slot></button>
       <dialog class="is-loading" part="dialog">
-        <div class="loading-visual" part="loading-visu
+        <div class="loading-visual" part="loading-visual"></div>
+        <div id="bars"></div>
+     
