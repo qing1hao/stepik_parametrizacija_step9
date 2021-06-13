d9891ef681cbe3fe4d53ac5ab9687a47c652797d
qing1hao <124031296+qing1hao@users.noreply.github.com>
2021-06-13 10:49:54 +0000

    fixed tests

--- src/index.ts
+++ src/index.ts
@@ -422,4 +422,4 @@ class OpenStoriesElement extends HTMLElement {
           </div>
           <div id="side-controls">
             <button id="close" class="action" type="button" aria-label="Close">
-      
+              <svg width="20" height="20" viewBox="0 0 20 20" fill=
