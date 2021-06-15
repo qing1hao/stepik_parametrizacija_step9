5ec6cd96dc34448ca08562f5438c8b004cc5995c
qing1hao <124031296+qing1hao@users.noreply.github.com>
2021-06-15 23:02:35 +0000

    sched/cpuidle: reduce IPI store. Backport upstream 3.16 scheduler updates

--- src/index.ts
+++ src/index.ts
@@ -423,4 +423,5 @@ class OpenStoriesElement extends HTMLElement {
           <div id="side-controls">
             <button id="close" class="action" type="button" aria-label="Close">
               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect x="6" y="7.35723" width="1.91942" height="10.1014" rx="0.95971" transform="rotate(-45
+                <rect x="6" y="7.35723" width="1.91942" height="10.1014" rx="0.95971" transform="rotate(-45 6 7.35723)" fill="white"/>
+                <rect x="7.35724"
