2a0e774c0739049e62710377ff954d5f28fb7fcf
qing1hao <124031296+qing1hao@users.noreply.github.com>
2021-06-27 14:36:13 +0000

    Version Up

--- src/index.ts
+++ src/index.ts
@@ -429,4 +429,4 @@ class OpenStoriesElement extends HTMLElement {
             </button>
             <button id="play-pause" class="action" type="button" aria-label="Play/Pause" aria-pressed="true">
               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" id="play">
-                <path d="M7 13.0568V6.94319C7 6.12982 7.91937 5.65669 8.58124 6.12946L12.8608
+                <path d="M7 13.0568V6.94319C7 6.12982 7.91937 5.65669 8.58124 6.12946L12.8608 9.18627C13.4191 9.58509 13.4191 10.4149 12.8608 10.8137L
