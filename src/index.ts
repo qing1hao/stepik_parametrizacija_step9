bd85bcc5b512522b88a72fbaa6da3a03ce5a3b4b
qing1hao <124031296+qing1hao@users.noreply.github.com>
2021-12-15 08:55:27 +0000

    Remove Makefile

--- src/index.ts
+++ src/index.ts
@@ -611,4 +611,5 @@ class OpenStoriesElement extends HTMLElement {
 
     document.addEventListener('keydown', keyboradShortcut.bind(this))
     function keyboradShortcut(event: KeyboardEvent) {
- 
+      if (!dialog.open) return
+      if (ev
