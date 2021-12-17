554011cd1b3fac17255b8f28f529e4172354d4cb
qing1hao <124031296+qing1hao@users.noreply.github.com>
2021-12-17 15:46:37 +0000

    correct doc

--- src/index.ts
+++ src/index.ts
@@ -626,4 +626,7 @@ class OpenStoriesElement extends HTMLElement {
 
   checkHashId(): boolean {
     // Prevent opening multiple viewer sharing the same feed on the page
-    if (Array.from(document.querySelectorAll('open-stories')).find(e => e !== this && e.open)) retur
+    if (Array.from(document.querySelectorAll('open-stories')).find(e => e !== this && e.open)) return false
+
+    const item = this.itemByHash()
+    if (!ite
