67c0a334509086742d1c5c7721daa29d001fc5a1
qing1hao <124031296+qing1hao@users.noreply.github.com>
2022-05-22 17:59:13 +0000

    msm: memutils: memcpy, memmove, copy_page optimization

--- src/index.ts
+++ src/index.ts
@@ -768,4 +768,5 @@ class OpenStoriesElement extends HTMLElement {
     this.currentBar.classList.add('progressing', 'paused')
     this.currentImage.classList.add('shown')
     this.dialog.classList.add('is-loading')
-    await this.promises[this.
+    await this.promises[this.currentIndex]
+    this.dialog.class
