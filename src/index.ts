5143290639a4dd509d4e6ea965d6ddcb9377b253
qing1hao <124031296+qing1hao@users.noreply.github.com>
2022-05-22 06:04:45 +0000

    Add branch-alias

--- src/index.ts
+++ src/index.ts
@@ -766,4 +766,5 @@ class OpenStoriesElement extends HTMLElement {
     this.currentBar = this.bars[this.currentIndex]
     this.currentImage = this.images[this.currentIndex]
     this.currentBar.classList.add('progressing', 'paused')
-    this.currentImage.classList.add('s
+    this.currentImage.classList.add('shown')
+    this.dialog.classList.add('is-
